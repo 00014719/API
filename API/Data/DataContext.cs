2753d67b73fe350db5da3f7f025822af21d65cf3
00014719 <117642084+00014719@users.noreply.github.com>
2024-10-27 16:50:45 +0500

    api

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) 
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
